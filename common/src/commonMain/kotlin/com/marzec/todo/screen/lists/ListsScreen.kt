145190a129bdec3c98dcc18dfb2bd4cda45a7f7e
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-04-03 15:08:47 +0200

    Title fix

--- common/src/commonMain/kotlin/com/marzec/todo/screen/lists/ListsScreen.kt
+++ common/src/commonMain/kotlin/com/marzec/todo/screen/lists/ListsScreen.kt
@@ -1,8 +1,7 @@
 package com.marzec.todo.screen.lists
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,16 +11,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.marzec.todo.navigation.model.NavigationActions
 import com.marzec.todo.navigation.model.NavigationStore
@@ -43,7 +39,7 @@ fun ListsScreen(navigationStore: NavigationStore, listsScreenStore: ListsScreenS
 
     Scaffold(
         topBar = {
-            Box(contentAlignment = Alignment.CenterStart) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 TextButton({
                     scope.launch { navigationStore.sendAction(NavigationActions.Back) }
                 }) {
