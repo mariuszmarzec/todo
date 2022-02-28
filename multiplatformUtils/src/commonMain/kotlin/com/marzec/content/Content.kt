19c860dfe4f2876804f0191bad11c0ab2b47b8f6
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-02-28 17:35:22 +0100

    Added utils methods

--- multiplatformUtils/src/commonMain/kotlin/com/marzec/content/Content.kt
+++ multiplatformUtils/src/commonMain/kotlin/com/marzec/content/Content.kt
@@ -2,6 +2,7 @@ package com.marzec.content
 
 import com.marzec.logger.Logger
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 
 sealed class Content<T> {
@@ -51,3 +52,85 @@ suspend fun <T> Content<T>.ifFinished(action: suspend Content<T>.() -> Unit) {
 }
 
 fun <T> Content.Error<T>.getMessage(): String = exception.message.orEmpty()
+
+fun <R> combineContents(vararg contents: Content<*>, mapData: (List<*>) -> R): Content<R> =
+    with(contents.toList()) {
+        val errorContent = firstOrNull { it is Content.Error<*> } as? Content.Error
+        if (errorContent != null) {
+            return Content.Error(errorContent.exception)
+        }
+        val loadingContent = firstOrNull { it is Content.Loading<*> } as? Content.Loading
+        if (loadingContent != null) {
+            return Content.Loading()
+        }
+        return Content.Data(mapData(filterIsInstance<Content.Data<*>>().map { it.data }))
+    }
+
+@Suppress("unchecked_cast", "MagicNumber")
+fun <T1, T2, R> combineContentsFlows(
+    flow: Flow<Content<T1>>,
+    flow2: Flow<Content<T2>>,
+    mapData: (T1, T2) -> R
+) = combine(
+    flow = flow,
+    flow2 = flow2,
+    transform = { content1, content2 ->
+        combineContents(content1, content2) { dataList ->
+            mapData(
+                dataList[0] as T1,
+                dataList[1] as T2
+            )
+        }
+    }
+)
+
+@Suppress("unchecked_cast", "MagicNumber")
+fun <T1, T2, T3, T4, R> combineContentsFlows(
+    flow: Flow<Content<T1>>,
+    flow2: Flow<Content<T2>>,
+    flow3: Flow<Content<T3>>,
+    flow4: Flow<Content<T4>>,
+    mapData: (T1, T2, T3, T4) -> R
+) = combine(
+    flow = flow,
+    flow2 = flow2,
+    flow3 = flow3,
+    flow4 = flow4,
+    transform = { content1, content2, content3, content4 ->
+        combineContents(content1, content2, content3, content4) { dataList ->
+            mapData(
+                dataList[0] as T1,
+                dataList[1] as T2,
+                dataList[2] as T3,
+                dataList[3] as T4
+            )
+        }
+    }
+)
+
+@Suppress("unchecked_cast", "MagicNumber")
+fun <T1, T2, T3, T4, T5, R> combineContentsFlows(
+    flow: Flow<Content<T1>>,
+    flow2: Flow<Content<T2>>,
+    flow3: Flow<Content<T3>>,
+    flow4: Flow<Content<T4>>,
+    flow5: Flow<Content<T5>>,
+    mapData: (T1, T2, T3, T4, T5) -> R
+) = combine(
+    flow = flow,
+    flow2 = flow2,
+    flow3 = flow3,
+    flow4 = flow4,
+    flow5 = flow5,
+    transform = { content1, content2, content3, content4, content5 ->
+        combineContents(content1, content2, content3, content4, content5) { dataList ->
+            mapData(
+                dataList[0] as T1,
+                dataList[1] as T2,
+                dataList[2] as T3,
+                dataList[3] as T4,
+                dataList[4] as T5
+            )
+        }
+    }
+)
