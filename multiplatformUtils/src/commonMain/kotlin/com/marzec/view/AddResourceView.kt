cad96a5023f6d4ff82b8b08ecc63486c38e5db41
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-05-03 08:24:30 +0200

    Added imports

--- multiplatformUtils/src/commonMain/kotlin/com/marzec/view/AddResourceView.kt
+++ multiplatformUtils/src/commonMain/kotlin/com/marzec/view/AddResourceView.kt
@@ -1,5 +1,17 @@
 package com.marzec.view
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
 @Composable
 private fun AddResourceView(
     showEmptyState: Boolean,
