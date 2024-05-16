a35880eabd898271124e0b4262fd7b43139ac891
Mariusz Marzec <mariusz.marzec00@gmail.com>
2024-05-16 12:35:54 +0200

    Improved state reducing in case of nested delegate

--- common/src/commonMain/kotlin/com/marzec/todo/delegates/reorder/ReorderDelegate.kt
+++ common/src/commonMain/kotlin/com/marzec/todo/delegates/reorder/ReorderDelegate.kt
@@ -1,13 +1,9 @@
 package com.marzec.todo.delegates.reorder
 
 import com.marzec.delegate.StoreDelegate
-import com.marzec.mvi.Intent3
-import com.marzec.mvi.IntentBuilder
-import com.marzec.mvi.IntentContext
 import com.marzec.mvi.State
 import com.marzec.mvi.map
 import com.marzec.mvi.mapToState
-import com.marzec.mvi.reduceData
 import com.marzec.todo.screen.tasks.model.TasksScreenState
 import kotlinx.coroutines.flow.flowOf
 
@@ -22,15 +18,17 @@ interface ReorderDelegate {
 
 class ReorderDelegateImpl : StoreDelegate<State<TasksScreenState>>(), ReorderDelegate {
 
-    private val stateMapper: (TasksScreenState) -> ReorderMode? = { it.reorderMode }
+    private val outStateReducer: TasksScreenState.(ReorderMode) -> TasksScreenState = { copy(reorderMode = it) }
+    private val outToInState: (TasksScreenState) -> ReorderMode? = { it.reorderMode }
 
     override fun enableReorderMode() {
         run(
             enableReorderModeIntent().map(
                 stateReducer = {
-                    state.copy(reorderMode = it(result, state.reorderMode))
+                    val newReorderModeState = it(result, state.reorderMode)
+                    state.outStateReducer(newReorderModeState)
                 },
-                stateMapper = stateMapper
+                stateMapper = outToInState
             ) {
                 onTrigger {
                     flowOf(state.tasks)
@@ -43,9 +41,9 @@ class ReorderDelegateImpl : StoreDelegate<State<TasksScreenState>>(), ReorderDel
         run(
             disableReorderModeIntent().map(
                 stateReducer = {
-                    state.copy(reorderMode = it(result, state.reorderMode))
+                    state.outStateReducer(it(result, state.reorderMode))
                 },
-                stateMapper = stateMapper
+                stateMapper = outToInState
             ).mapToState()
         )
     }
@@ -54,9 +52,9 @@ class ReorderDelegateImpl : StoreDelegate<State<TasksScreenState>>(), ReorderDel
         run(
             onDraggedIntent(draggedIndex, targetIndex).map(
                 stateReducer = {
-                    state.copy(reorderMode = it(result, state.reorderMode))
+                    state.outStateReducer(it(result, state.reorderMode))
                 },
-                stateMapper = stateMapper
+                stateMapper = outToInState
             ).mapToState()
         )
     }
