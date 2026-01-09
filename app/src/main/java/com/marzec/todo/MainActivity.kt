5a098313c11f46bb9f97f70a68ec142c377ffe1e
Mariusz Marzec <mariusz.marzec00@gmail.com>
2026-01-09 01:03:45 +0100

    Asking for permissions

--- app/src/main/java/com/marzec/todo/MainActivity.kt
+++ app/src/main/java/com/marzec/todo/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.marzec.todo
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -11,13 +15,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import com.marzec.common.OpenUrlHelper
 import com.marzec.todo.screen.main.HomeScreenSaveable
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            askNotificationPermission()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        askNotificationPermission()
 
         DI.openUrlHelper = object : OpenUrlHelper {
             override fun open(url: String) {
@@ -37,6 +55,19 @@ class MainActivity : AppCompatActivity() {
 
                     HomeScreenSaveable()
                 }
+            }
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
