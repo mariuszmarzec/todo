2d59fa80ea17f6b077309e6e08d9705228168b35
Mariusz Marzec <mariusz.marzec00@gmail.com>
2026-02-02 23:51:55 +0100

    Removed default credentials

--- common/src/commonMain/kotlin/com/marzec/todo/screen/login/model/LoginData.kt
+++ common/src/commonMain/kotlin/com/marzec/todo/screen/login/model/LoginData.kt
@@ -7,8 +7,8 @@ data class LoginData(val login: String, val password: String) {
     companion object {
         val INITIAL = State.Data(
             LoginData(
-                login = "mariusz.marzec00@gmail.com",
-                password = "password"
+                login = "",
+                password = ""
             )
         )
     }
