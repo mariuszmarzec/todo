c1eb96d91a9a47344d9ea86d21cdcb14e0f8de40
manul <opencode@ai.local>
2026-08-21 16:44:01 +0200

    fix: revert all api changes except adding api(project(":navigation"))
    Signed-off-by: manul <opencode@ai.local>

--- multiplatformUtils/build.gradle.kts
+++ multiplatformUtils/build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
                 api(libs.serializationJson)
                 api(libs.quickMvi)
                 api(libs.quickMvi.compose)
+                api(project(":navigation"))
                 implementation(libs.kotlinDateTime)
                 implementation(libs.compose.icons)
             }
