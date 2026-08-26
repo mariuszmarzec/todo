05ad8679fa6012a8acb23f38beb6f0d14b0387a6
mariuszmarzec <41124722+mariuszmarzec@users.noreply.github.com>
2026-08-26 15:56:37 +0200

    Removed unnecessary line

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -26,7 +26,6 @@ kotlin {
 compose.desktop {
     application {
         mainClass = "com.marzec.todo.MainKt"
-        println(System.getenv("JDK_17"))
 
         nativeDistributions {
             version = "1.0.0"
